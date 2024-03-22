5f163a59b012751130317eacae7dfa000c5c9abd
seokhoonj <seokhoonj@gmail.com>
2024-03-22 16:00:35 +0900

    `metaplot()` function is added

--- R/ggshort-package.R
+++ R/ggshort-package.R
@@ -5,8 +5,8 @@
 #' @keywords internal
 #' @import ggplot2
 #' @importFrom gridExtra arrangeGrob grid.arrange
-#' @importFrom data.table `:=` `.SD` as.data.table data.table set setDT
-#' @importFrom jaid paste_list set_dt unilen
+#' @importFrom data.table `:=` `.SD` as.data.table data.table melt set setDT
+#' @importFrom jaid assert_class meta paste_list set_dt unilen
 #' @importFrom plotly ggplotly plot_ly
 #' @importFrom rlang enquo enquos quo_is_null
 #' @importFrom scales comma
---
+++ R/meta.R
@@ -0,0 +1,62 @@
+#' Meta plot
+#'
+#' Draw a plot for a meta object.
+#'
+#' @param x a meta object
+#' @param widths a unit vector giving the width of each column
+#' @param theme a string specifying a ggshort theme function ("view", "save", "shiny")
+#' @return a ggplot object
+#'
+#' @examples
+#' # meta plot
+#' \dontrun{
+#' mt <- jaid::meta(mtcars)
+#' metaplot(mt)}
+#'
+#' @export
+metaplot <- function(x, widths = c(5, 5), theme = c("view", "save", "shiny")) {
+  jaid::assert_class(x, "meta")
+  column <- value <- variable <- NULL
+
+  x$distinct <- as.character(x$distinct)
+  x$mode <- as.character(x$mode)
+
+  dt <- melt(as.data.table(x), id.vars = c("column"),
+             measure.vars = c("prop", "nzprop"))
+  dt$column <- factor(dt$column, levels = sort(unique(dt$column)))
+  xlvl <- levels(dt$column)
+  dt$column <- as.numeric(dt$column)
+
+  ds <- melt(as.data.table(x), id.vars = c("column"),
+             measure.vars = c("class", "distinct", "mode"))
+  ds$info <- "info"
+
+  theme <- match.arg(theme)
+  p <- gridExtra::arrangeGrob(
+    ggtable(ds, x = variable, y = column, label = value,
+            xlab_position = "bottom", linetype = "solid") +
+      facet_wrap(~ info) +
+      xlab("") +
+      ylab("") +
+      ggshort_theme(theme = theme),
+    ggbar(dt, x = column, y = value, ymax = 1.5,
+          label = sprintf("%.3f", value), label_hjust = -.1) +
+      geom_vline(xintercept = seq(1, 1 + length(xlvl)) - 0.5, linetype = "solid") +
+      scale_x_reverse(breaks = seq(1, length(xlvl)), labels = xlvl) +
+      scale_y_continuous(breaks = c(0, .5, 1, 1.5)) +
+      coord_flip() +
+      facet_wrap(~ variable) +
+      xlab("") +
+      ylab("") +
+      ggshort_theme(theme = theme, y.size = 0),
+    ncol = 2L, widths = widths
+  )
+  gridExtra::grid.arrange(p)
+}
+
+#' @method plot meta
+#' @export
+plot.meta <- function(x, widths = c(5, 5), theme = c("view", "save", "shiny"), ...) {
+  theme <- match.arg(theme)
+  metaplot(x = x, widths = widths, theme = theme)
+}
